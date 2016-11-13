fd5c214186fcca2a90d165ba977e16c0a7a433cb
Doh09 <alexandreasen09@gmail.com>
2016-11-13 12:51:56 +0100

    Added - A currency conversion method to the Movie entity. - A dropdown button that displays the total price of an order in DKK, USD and EUR, at checkout.

--- BlockFlixWeb/BlockFlixDLL/Entities/Movie.cs
+++ BlockFlixWeb/BlockFlixDLL/Entities/Movie.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using ExchangeRate;
 
 namespace BlockFlixDLL.Entities
 {
@@ -14,5 +15,16 @@ namespace BlockFlixDLL.Entities
         public string ImageURL { get; set; }
         public string TrailerURL { get; set; }
         public virtual List<Genre> Genres { get; set; }
+
+        /// <summary>
+        /// This method uses the nuget package "exchange-rate" to convert the movies price into another currency than the base currency
+        /// of DKK, the exchange rate provider is Google, the calculated new currency is returned as a 'double' value.
+        /// </summary>
+        /// <param name="newCurrency"></param>
+        /// <returns></returns>
+        public double GetPriceInDifferentCurrency(Iso4217 newCurrency)
+        {
+            return Provider.Google.Convert(Iso4217.DKK, newCurrency, Price);
+        }
     }
 }
---
+++ BlockFlixWeb/UnitTests/TestMovieEntity.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using BlockFlixDLL.Entities;
+using ExchangeRate;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UnitTests
+{
+    [TestClass]
+    public class TestMovieEntity
+    {
+        [TestMethod]
+        public void TestCurrencyConversion()
+        {
+            Movie m = new Movie()
+            {
+                ID = 0,
+                Price = 100
+            };
+            double priceInUSD = m.GetPriceInDifferentCurrency(Iso4217.USD);
+            double priceInEUR = m.GetPriceInDifferentCurrency(Iso4217.EUR);
+            //Check that the 3 prices are not the same value.
+            Assert.AreNotEqual(priceInUSD, priceInEUR);
+            Assert.AreNotEqual(priceInUSD, m.Price);
+            Assert.AreNotEqual(priceInEUR, m.Price);
+            //Test if the prices can be converted again and still give aproximately the same value.
+            Assert.IsTrue(Math.Abs((Provider.Google.Convert(Iso4217.DKK, Iso4217.EUR, 100)) - priceInEUR) < 0.1 );
+            Assert.IsTrue(Math.Abs((Provider.Google.Convert(Iso4217.DKK, Iso4217.USD, 100)) - priceInUSD) < 0.1);
+        }
+    }
+}
