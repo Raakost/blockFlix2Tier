4817e2f04012f953926528321e264accb274245c
Doh09 <alexandreasen09@gmail.com>
2016-11-18 15:27:20 +0100

    Added new currency converter solution and removed old one.

---
+++ BlockFlixWeb/BlockFlixDLL/Entities/Currency.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+namespace BlockFlixDLL.Entities
+{
+    public class Currency
+    {
+        [JsonProperty("DKK")]
+        public double DanishCurrency { get; set; }
+        [JsonProperty("USD")]
+        public double USCurrency { get; set; }
+    }
+}
---
+++ BlockFlixWeb/BlockFlixDLL/GatewayServices/ExchangeRateGateway.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Threading.Tasks;
+using BlockFlixDLL.Entities;
+using Newtonsoft.Json;
+
+namespace BlockFlixDLL.GatewayServices
+{
+    public class ExchangeRateGateway
+    {
+        private static void SetUpClientConnection(HttpClient client)
+        {
+            client.BaseAddress = new Uri("http://api.fixer.io");
+            client.DefaultRequestHeaders.Accept.Clear();
+            client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+        }
+
+        public static CurrencyJSON GetAll()
+        {
+            using (var client = new HttpClient())
+            {
+                SetUpClientConnection(client);
+                HttpResponseMessage response = client.GetAsync("/latest?symbols=USD,DKK").Result;
+                if (response.IsSuccessStatusCode)
+                {
+                    CurrencyJSON currency;
+                    //string Json = response.Content.ReadAsStringAsync().Result;
+                    currency = JsonConvert.DeserializeObject<CurrencyJSON>(response.Content.ReadAsStringAsync().Result);
+                    return currency;
+                }
+                return null;
+            }
+        }
+    }
+}
