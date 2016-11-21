84f5a69765d5b68804e57187ae19bbbb726f4f6f
Doh09 <alexandreasen09@gmail.com>
2016-11-21 08:59:11 +0100

    Changed unit test.

--- BlockFlixWeb/UnitTests/TestMovieEntity.cs
+++ BlockFlixWeb/UnitTests/TestMovieEntity.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using BlockFlixDLL.Entities;
+using BlockFlixDLL.GatewayServices;
 using ExchangeRate;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
@@ -17,15 +18,21 @@ namespace UnitTests
                 ID = 0,
                 Price = 100
             };
-            double priceInUSD = m.GetPriceInDifferentCurrency(Iso4217.USD);
-            double priceInEUR = m.GetPriceInDifferentCurrency(Iso4217.EUR);
+            CurrencyJSON currencyJson = ExchangeRateGateway.GetAll();
+            double USDrate = currencyJson.Currencies.USCurrency;
+            double EURrate = currencyJson.Currencies.DanishCurrency;
+            double priceInUSD = m.Price*USDrate;
+            double priceInEUR = m.Price*EURrate;
             //Check that the 3 prices are not the same value.
             Assert.AreNotEqual(priceInUSD, priceInEUR);
             Assert.AreNotEqual(priceInUSD, m.Price);
             Assert.AreNotEqual(priceInEUR, m.Price);
             //Test if the prices can be converted again and still give aproximately the same value.
-            Assert.IsTrue(Math.Abs((Provider.Google.Convert(Iso4217.DKK, Iso4217.EUR, 100)) - priceInEUR) < 0.1 );
-            Assert.IsTrue(Math.Abs((Provider.Google.Convert(Iso4217.DKK, Iso4217.USD, 100)) - priceInUSD) < 0.1);
+            currencyJson = ExchangeRateGateway.GetAll();
+            USDrate = currencyJson.Currencies.USCurrency;
+            EURrate = currencyJson.Currencies.DanishCurrency;
+            Assert.IsTrue(Math.Abs(m.Price * EURrate - priceInEUR) < 0.1 );
+            Assert.IsTrue(Math.Abs(m.Price * USDrate - priceInUSD) < 0.1);
         }
     }
 }
